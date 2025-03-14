fe6d3021259a7905a5734406b48d6647473dc37b
Juna <juna.ginter@web.de>
2025-03-14 11:27:41 +0100

    added move logic except pawns

--- src/pieceContainer.py
+++ src/pieceContainer.py
@@ -2,6 +2,12 @@ from quantumPiece import QuantumPiece
 import random
 
 
+def inRange(coordinate):
+    if coordinate[0] < 0 or coordinate[0] > 7 or coordinate[1] < 0 or coordinate[1] > 7:
+        return False
+    return True
+
+
 class PieceContainer:
     
     def __init__(self):
@@ -54,19 +60,30 @@ class PieceContainer:
 
     def capturePiece(self, coordinate):
         capturedPiece = self.getPiece(coordinate)
-        if not capturedPiece is None:
+        if capturedPiece is not None:
             if random.random() < capturedPiece.coordinates[coordinate]:
                 capturedPiece.die()
             else:
                 capturedPiece.coordinates.pop(coordinate)
                 capturedPiece.adjustProbabilities()
 
+    def addMovesInLine(self, coordinate, piece, xDirection, yDirection):
+        moves = []
+        checkTile = (coordinate[0] + xDirection, coordinate[1] + yDirection)
+        while inRange(checkTile):
+            if not self.getPiece(checkTile).color == piece.color:
+                moves.append(checkTile)
+                checkTile = (checkTile[0] + xDirection, checkTile[1] + yDirection)
+            else:
+                break
+        return moves
+
     def getMoves(self, coordinate):
         piece = self.getPiece(coordinate)
+        coordinates = []
         match piece.type:
             case "King":
                 # collects all theoretically possible moves
-                coordinates = []
                 for x in (-1, 0, 1):
                     for y in (-1, 0, 1):
                         coordinates.append((coordinate[0] + x, coordinate[1] + y))
@@ -74,7 +91,7 @@ class PieceContainer:
 
                 # removes all moves out of bound
                 for x in coordinates:
-                    if coordinate[0] < 0 or coordinate[0] > 7 or coordinate[1] < 0 or coordinate[1] > 7:
+                    if not inRange(x):
                         coordinates.remove(x)
                     # removes all moves to tiles inhibited by ur own pieces
                     else:
@@ -90,11 +107,30 @@ class PieceContainer:
                         if self.getPiece((6, coordinate[1])) is None:
                             coordinates.append((6, coordinate[1]))
 
-                return coordinates
             case "Queen":
+                for xDirection in (-1, 0, 1):
+                    for yDirection in (-1, 0, 1):
+                        if xDirection == yDirection == 0:
+                            continue
+                        coordinates.extend(self.addMovesInLine(coordinate, piece, xDirection, yDirection))
 
             case "Bishop":
+                for xDirection in (-1, 1):
+                    for yDirection in (-1, 1):
+                        coordinates.extend(self.addMovesInLine(coordinate, piece, xDirection, yDirection))
+
+            case "Rook":
+                for xDirection, yDirection in ((-1, 0), (1, 0), (0, -1), (0, 1)):
+                    coordinates.extend(self.addMovesInLine(coordinate, piece, xDirection, yDirection))
 
             case "Knight":
+                for x in (-1, 1):
+                    for y in (-1, 1):
+                        for checkTile in ((2 * x, y), (x, 2 * y)):
+                            if inRange(checkTile) and not self.getPiece(checkTile).color == piece.color:
+                                coordinates.append(checkTile)
 
             case "Pawn":
+                pass
+
+        return coordinates
